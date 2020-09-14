150d7457710a1690a7ed616378f69a3bdf9e98b1
Michał <mkozak@man.poznan.pl>
2020-09-14 14:53:07 +0200

    migrations for publication properties and tests

--- src/test/java/eu/sshopencloud/marketplace/controllers/vocabularies/PropertyTypeControllerITCase.java
+++ src/test/java/eu/sshopencloud/marketplace/controllers/vocabularies/PropertyTypeControllerITCase.java
@@ -56,38 +56,37 @@ public class PropertyTypeControllerITCase {
         )
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.hits", is(18)))
+                .andExpect(jsonPath("$.hits", is(26)))
                 .andExpect(jsonPath("$.count", is(10)))
                 .andExpect(jsonPath("$.page", is(1)))
                 .andExpect(jsonPath("$.perpage", is(10)))
-                .andExpect(jsonPath("$.pages", is(2)))
+                .andExpect(jsonPath("$.pages", is(3)))
                 .andExpect(jsonPath("$.propertyTypes", hasSize(10)))
                 .andExpect(jsonPath("$.propertyTypes[*].code", contains(
-                        "object-type", "language", "activity", "technique", "material", "object-format",
-                        "keyword", "tadirah-goals", "thumbnail", "repository-url"
+                        "language", "activity", "technique", "material", "object-format",
+                        "keyword", "tadirah-goals", "thumbnail", "repository-url", "license-type"
                 )))
-                .andExpect(jsonPath("$.propertyTypes[5].label", is("Object format")))
-                .andExpect(jsonPath("$.propertyTypes[1].allowedVocabularies", hasSize(2)))
-                .andExpect(jsonPath("$.propertyTypes[1].allowedVocabularies[*].code", contains("iso-639-3", "iso-639-3-v2")))
+                .andExpect(jsonPath("$.propertyTypes[5].label", is("Keyword")))
+                .andExpect(jsonPath("$.propertyTypes[0].allowedVocabularies", hasSize(2)))
+                .andExpect(jsonPath("$.propertyTypes[0].allowedVocabularies[*].code", contains("iso-639-3", "iso-639-3-v2")))
                 .andExpect(jsonPath("$.propertyTypes[7].ord", is(8)));
 
         mvc.perform(
                 get("/api/property-types")
-                        .param("page", "2")
+                        .param("page", "3")
                         .param("perpage", "10")
                         .accept(MediaType.APPLICATION_JSON)
         )
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.hits", is(18)))
-                .andExpect(jsonPath("$.count", is(8)))
-                .andExpect(jsonPath("$.page", is(2)))
+                .andExpect(jsonPath("$.hits", is(26)))
+                .andExpect(jsonPath("$.count", is(6)))
+                .andExpect(jsonPath("$.page", is(3)))
                 .andExpect(jsonPath("$.perpage", is(10)))
-                .andExpect(jsonPath("$.pages", is(2)))
-                .andExpect(jsonPath("$.propertyTypes", hasSize(8)))
+                .andExpect(jsonPath("$.pages", is(3)))
+                .andExpect(jsonPath("$.propertyTypes", hasSize(6)))
                 .andExpect(jsonPath("$.propertyTypes[*].code", contains(
-                        "license-type", "web-usable", "tool-family", "tadirah-methods", "methodica-link",
-                        "cover-image", "media", "see-also"
+                        "issue", "pages", "year", "timestamp", "publication-type", "doi"
                 )));
     }
 
@@ -111,7 +110,7 @@ public class PropertyTypeControllerITCase {
                 .andExpect(jsonPath("$.code", is("new-property-type")))
                 .andExpect(jsonPath("$.label", is("New property type")))
                 .andExpect(jsonPath("$.type", is("concept")))
-                .andExpect(jsonPath("$.ord", is(19)))
+                .andExpect(jsonPath("$.ord", is(27)))
                 .andExpect(jsonPath("$.allowedVocabularies", hasSize(2)))
                 .andExpect(jsonPath("$.allowedVocabularies[*].code", containsInAnyOrder("nemo-activity-type", "iana-mime-type")));
 
@@ -124,7 +123,7 @@ public class PropertyTypeControllerITCase {
                 .andExpect(jsonPath("$.code", is("new-property-type")))
                 .andExpect(jsonPath("$.label", is("New property type")))
                 .andExpect(jsonPath("$.type", is("concept")))
-                .andExpect(jsonPath("$.ord", is(19)))
+                .andExpect(jsonPath("$.ord", is(27)))
                 .andExpect(jsonPath("$.allowedVocabularies", hasSize(2)))
                 .andExpect(jsonPath("$.allowedVocabularies[*].code", containsInAnyOrder("nemo-activity-type", "iana-mime-type")));
     }
@@ -158,7 +157,7 @@ public class PropertyTypeControllerITCase {
                 .andExpect(jsonPath("$.code", is("language")))
                 .andExpect(jsonPath("$.label", is("Language code")))
                 .andExpect(jsonPath("$.type", is("concept")))
-                .andExpect(jsonPath("$.ord", is(2)))
+                .andExpect(jsonPath("$.ord", is(1)))
                 .andExpect(jsonPath("$.allowedVocabularies", hasSize(2)))
                 .andExpect(jsonPath("$.allowedVocabularies[*].code", containsInAnyOrder("iso-639-3-v2", "software-license")));
 
@@ -171,19 +170,19 @@ public class PropertyTypeControllerITCase {
                 .andExpect(jsonPath("$.code", is("language")))
                 .andExpect(jsonPath("$.label", is("Language code")))
                 .andExpect(jsonPath("$.type", is("concept")))
-                .andExpect(jsonPath("$.ord", is(2)))
+                .andExpect(jsonPath("$.ord", is(1)))
                 .andExpect(jsonPath("$.allowedVocabularies", hasSize(2)))
                 .andExpect(jsonPath("$.allowedVocabularies[*].code", containsInAnyOrder("iso-639-3-v2", "software-license")));
     }
 
     @Test
     public void shouldDeletePropertyType() throws Exception {
-        assertPropertyTypeOrder("activity", 3);
-        assertPropertyTypeOrder("technique", 4);
-        assertPropertyTypeOrder("web-usable", 12);
-        assertPropertyTypeOrder("tool-family", 13);
-        assertPropertyTypeOrder("media", 17);
-        assertPropertyTypeOrder("license-type", 11);
+        assertPropertyTypeOrder("activity", 2);
+        assertPropertyTypeOrder("technique", 3);
+        assertPropertyTypeOrder("web-usable", 11);
+        assertPropertyTypeOrder("tool-family", 12);
+        assertPropertyTypeOrder("media", 16);
+        assertPropertyTypeOrder("license-type", 10);
 
         mvc.perform(
                 delete("/api/property-types/{code}", "technique")
@@ -199,28 +198,28 @@ public class PropertyTypeControllerITCase {
                 .andExpect(status().isNotFound());
 
 
-        assertPropertyTypeOrder("activity", 3);
-        assertPropertyTypeOrder("web-usable", 11);
-        assertPropertyTypeOrder("tool-family", 12);
-        assertPropertyTypeOrder("media", 16);
-        assertPropertyTypeOrder("license-type", 10);
+        assertPropertyTypeOrder("activity", 2);
+        assertPropertyTypeOrder("web-usable", 10);
+        assertPropertyTypeOrder("tool-family", 11);
+        assertPropertyTypeOrder("media", 15);
+        assertPropertyTypeOrder("license-type", 9);
     }
 
     @Test
     public void shouldProperlyReorderPropertyTypes() throws Exception {
-        assertPropertyTypeOrder("object-format", 6);
-        assertPropertyTypeOrder("keyword", 7);
-        assertPropertyTypeOrder("tadirah-goals", 8);
-        assertPropertyTypeOrder("thumbnail", 9);
-        assertPropertyTypeOrder("repository-url", 10);
-        assertPropertyTypeOrder("license-type", 11);
+        assertPropertyTypeOrder("object-format", 5);
+        assertPropertyTypeOrder("keyword", 6);
+        assertPropertyTypeOrder("tadirah-goals", 7);
+        assertPropertyTypeOrder("thumbnail", 8);
+        assertPropertyTypeOrder("repository-url", 9);
+        assertPropertyTypeOrder("license-type", 10);
 
         PropertyTypesReordering request = new PropertyTypesReordering(
                 Arrays.asList(
-                        new PropertyTypeReorder("repository-url", 7),
-                        new PropertyTypeReorder("thumbnail", 7),
-                        new PropertyTypeReorder("keyword", 10),
-                        new PropertyTypeReorder("tadirah-goals", 9)
+                        new PropertyTypeReorder("repository-url", 6),
+                        new PropertyTypeReorder("thumbnail", 6),
+                        new PropertyTypeReorder("keyword", 9),
+                        new PropertyTypeReorder("tadirah-goals", 8)
                 )
         );
 
@@ -232,29 +231,29 @@ public class PropertyTypeControllerITCase {
         )
                 .andExpect(status().isOk());
 
-        assertPropertyTypeOrder("object-format", 6);
-        assertPropertyTypeOrder("thumbnail", 7);
-        assertPropertyTypeOrder("repository-url", 8);
-        assertPropertyTypeOrder("tadirah-goals", 9);
-        assertPropertyTypeOrder("keyword", 10);
-        assertPropertyTypeOrder("license-type", 11);
+        assertPropertyTypeOrder("object-format", 5);
+        assertPropertyTypeOrder("thumbnail", 6);
+        assertPropertyTypeOrder("repository-url", 7);
+        assertPropertyTypeOrder("tadirah-goals", 8);
+        assertPropertyTypeOrder("keyword", 9);
+        assertPropertyTypeOrder("license-type", 10);
     }
 
     @Test
     public void shouldNotReorderWithOrdBelowBounds() throws Exception {
-        assertPropertyTypeOrder("object-format", 6);
-        assertPropertyTypeOrder("keyword", 7);
-        assertPropertyTypeOrder("tadirah-goals", 8);
-        assertPropertyTypeOrder("thumbnail", 9);
-        assertPropertyTypeOrder("repository-url", 10);
-        assertPropertyTypeOrder("license-type", 11);
+        assertPropertyTypeOrder("object-format", 5);
+        assertPropertyTypeOrder("keyword", 6);
+        assertPropertyTypeOrder("tadirah-goals", 7);
+        assertPropertyTypeOrder("thumbnail", 8);
+        assertPropertyTypeOrder("repository-url", 9);
+        assertPropertyTypeOrder("license-type", 10);
 
         PropertyTypesReordering request = new PropertyTypesReordering(
                 Arrays.asList(
-                        new PropertyTypeReorder("repository-url", 7),
-                        new PropertyTypeReorder("thumbnail", 7),
+                        new PropertyTypeReorder("repository-url", 6),
+                        new PropertyTypeReorder("thumbnail", 6),
                         new PropertyTypeReorder("tadirah-goals", 0),
-                        new PropertyTypeReorder("keyword", 10)
+                        new PropertyTypeReorder("keyword", 9)
                 )
         );
 
@@ -269,12 +268,12 @@ public class PropertyTypeControllerITCase {
 
     @Test
     public void shouldNotReorderUnauthorized() throws Exception {
-        assertPropertyTypeOrder("object-format", 6);
-        assertPropertyTypeOrder("keyword", 7);
-        assertPropertyTypeOrder("tadirah-goals", 8);
-        assertPropertyTypeOrder("thumbnail", 9);
-        assertPropertyTypeOrder("repository-url", 10);
-        assertPropertyTypeOrder("license-type", 11);
+        assertPropertyTypeOrder("object-format", 5);
+        assertPropertyTypeOrder("keyword", 6);
+        assertPropertyTypeOrder("tadirah-goals", 7);
+        assertPropertyTypeOrder("thumbnail", 8);
+        assertPropertyTypeOrder("repository-url", 9);
+        assertPropertyTypeOrder("license-type", 10);
 
         PropertyTypesReordering request = new PropertyTypesReordering(
                 Arrays.asList(
@@ -296,19 +295,19 @@ public class PropertyTypeControllerITCase {
 
     @Test
     public void shouldNotReorderWithNotExistentPropertyType() throws Exception {
-        assertPropertyTypeOrder("object-format", 6);
-        assertPropertyTypeOrder("keyword", 7);
-        assertPropertyTypeOrder("tadirah-goals", 8);
-        assertPropertyTypeOrder("thumbnail", 9);
-        assertPropertyTypeOrder("repository-url", 10);
-        assertPropertyTypeOrder("license-type", 11);
+        assertPropertyTypeOrder("object-format", 5);
+        assertPropertyTypeOrder("keyword", 6);
+        assertPropertyTypeOrder("tadirah-goals", 7);
+        assertPropertyTypeOrder("thumbnail", 8);
+        assertPropertyTypeOrder("repository-url", 9);
+        assertPropertyTypeOrder("license-type", 10);
 
         PropertyTypesReordering request = new PropertyTypesReordering(
                 Arrays.asList(
-                        new PropertyTypeReorder("repository-url", 7),
-                        new PropertyTypeReorder("thumbnail", 7),
-                        new PropertyTypeReorder("tadi-goals", 9),
-                        new PropertyTypeReorder("keyword", 10)
+                        new PropertyTypeReorder("repository-url", 6),
+                        new PropertyTypeReorder("thumbnail", 6),
+                        new PropertyTypeReorder("tadi-goals", 8),
+                        new PropertyTypeReorder("keyword", 9)
                 )
         );
 
@@ -323,19 +322,19 @@ public class PropertyTypeControllerITCase {
 
     @Test
     public void shouldNotReorderWithOrdOutOfBounds() throws Exception {
-        assertPropertyTypeOrder("object-format", 6);
-        assertPropertyTypeOrder("keyword", 7);
-        assertPropertyTypeOrder("tadirah-goals", 8);
-        assertPropertyTypeOrder("thumbnail", 9);
-        assertPropertyTypeOrder("repository-url", 10);
-        assertPropertyTypeOrder("license-type", 11);
+        assertPropertyTypeOrder("object-format", 5);
+        assertPropertyTypeOrder("keyword", 6);
+        assertPropertyTypeOrder("tadirah-goals", 7);
+        assertPropertyTypeOrder("thumbnail", 8);
+        assertPropertyTypeOrder("repository-url", 9);
+        assertPropertyTypeOrder("license-type", 10);
 
         PropertyTypesReordering request = new PropertyTypesReordering(
                 Arrays.asList(
-                        new PropertyTypeReorder("repository-url", 7),
-                        new PropertyTypeReorder("thumbnail", 7),
+                        new PropertyTypeReorder("repository-url", 6),
+                        new PropertyTypeReorder("thumbnail", 6),
                         new PropertyTypeReorder("tadirah-goals", 30),
-                        new PropertyTypeReorder("keyword", 10)
+                        new PropertyTypeReorder("keyword", 9)
                 )
         );
 
