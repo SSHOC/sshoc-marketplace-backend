df6297cf992e49e61eda956c0ca94cae02955a60
Michał <mkozak@man.poznan.pl>
2020-10-27 20:09:11 +0100

    #37

--- src/main/java/eu/sshopencloud/marketplace/controllers/auth/TokenController.java
+++ src/main/java/eu/sshopencloud/marketplace/controllers/auth/TokenController.java
@@ -2,6 +2,7 @@ package eu.sshopencloud.marketplace.controllers.auth;
 
 import eu.sshopencloud.marketplace.dto.auth.ImplicitGrantTokenData;
 import eu.sshopencloud.marketplace.dto.auth.OAuthRegistrationDto;
+import eu.sshopencloud.marketplace.dto.auth.UserDto;
 import eu.sshopencloud.marketplace.services.auth.InvalidTokenException;
 import eu.sshopencloud.marketplace.services.auth.TokenService;
 import eu.sshopencloud.marketplace.services.auth.UserService;
@@ -11,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/oauth")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class TokenController {
 
@@ -19,7 +20,12 @@ public class TokenController {
 
     private final UserService userService;
 
-    @PutMapping(path = "/token", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/auth/me", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDto> getLoggedInUser() {
+        return ResponseEntity.ok(userService.getLoggedInUser());
+    }
+
+    @PutMapping(path = "/oauth/token", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<OAuthRegistrationDto> validateImplicitGrantToken(@RequestBody ImplicitGrantTokenData implicitGrantTokenData) throws InvalidTokenException {
         String jwtToken = tokenService.validateImplicitGrantToken(implicitGrantTokenData.getToken());
         if (implicitGrantTokenData.isRegistration()) {
