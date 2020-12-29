1c0712a9fbc6f651e8c5cd480a1c46a7739de6c3
swolarz <swolarz@man.poznan.pl>
2020-12-29 16:19:25 +0100

    SSHOC-40 property values validation Add validation for non concept property values Validation is performed for string int float date and url values

--- src/main/java/eu/sshopencloud/marketplace/conf/datetime/ApiDateTimeFormatter.java
+++ src/main/java/eu/sshopencloud/marketplace/conf/datetime/ApiDateTimeFormatter.java
@@ -8,6 +8,8 @@ import java.time.format.DateTimeFormatter;
 @UtilityClass
 public class ApiDateTimeFormatter {
 
+    public final String SIMPLE_DATE_FORMAT = "yyyy-MM-dd";
+
     public final String inputDateTimePattern = "yyyy-MM-dd'T'HH:mm:ss[.SSS]XX";
 
     public final String inputDateTimeExample = "2000-02-29T20:02:00+0000";
@@ -15,6 +17,9 @@ public class ApiDateTimeFormatter {
     public final String outputDateTimePattern = "yyyy-MM-dd'T'HH:mm:ssZ";
 
     public final String outputDateTimeExample = "2000-02-29T20:02:00+0000";
+
+    public final DateTimeFormatter INPUT_DATE_FORMATTER = DateTimeFormatter.ofPattern(inputDateTimePattern);
+    public final DateTimeFormatter SIMPLE_DATE_FORMATTER = DateTimeFormatter.ofPattern(SIMPLE_DATE_FORMAT);
 
 
     public DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(outputDateTimePattern);
