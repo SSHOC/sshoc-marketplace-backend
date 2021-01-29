81ca0859f91f94422a7b044e39165ff085348b3d
Omar Siam <Omar.Siam@oeaw.ac.at>
2021-01-29 17:16:01 +0100

    For the parameter specifying the openapi.json we can't use a static string as we will have all kinds of hostnames, ports and http or https.

--- src/main/java/eu/sshopencloud/marketplace/controllers/RootRedirectController.java
+++ src/main/java/eu/sshopencloud/marketplace/controllers/RootRedirectController.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/")
 public class RootRedirectController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String toApiDocs() {
-        return "redirect:/swagger-ui/index.html?url=http://localhost:8080/v3/api-docs";
+    public String toApiDocs(HttpServletRequest request) {
+        return "redirect:/swagger-ui/index.html?url=" + request.getScheme() + "://" + request.getServerName() + ":"+ request.getServerPort() +"/v3/api-docs";
     }
 }
