1aedbf78a9cd18951b67b7de8eaa191fa30294d5
ekalata <ekalata@man.poznan.pl>
2021-06-22 13:42:46 +0200

    adding enum

---
+++ src/main/java/eu/sshopencloud/marketplace/model/items/ItemMediaType.java
@@ -0,0 +1,17 @@
+package eu.sshopencloud.marketplace.model.items;
+
+public enum ItemMediaType {
+    MEDIA("Media"),
+    THUMBNAIL("Thumbnail"),             //media that is also thumbnail
+    THUMBNAIL_ONLY("Thumbnail only");   //thumbnail but not media
+
+    private final String label;
+
+    ItemMediaType(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
