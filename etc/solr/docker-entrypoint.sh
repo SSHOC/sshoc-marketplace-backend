4f7bf5a3ded48d6339a929fd027c2c6cd2aafdf6
ekalata <ekalata@man.poznan.pl>
2021-09-14 08:15:25 +0200

    Changing files to adjust to updated Docker.

--- etc/solr/docker-entrypoint.sh
+++ etc/solr/docker-entrypoint.sh
@@ -4,20 +4,25 @@ SOLR_MARKETPLACE_ITEMS_DIR="/var/solr/data/marketplace-items"
 SOLR_MARKETPLACE_CONCEPTS_DIR="/var/solr/data/marketplace-concepts"
 SOLR_MARKETPLACE_ACTORS_DIR="/var/solr/data/marketplace-actors"
 
-
 if [ ! -d $SOLR_MARKETPLACE_ITEMS_DIR ]; then
-	mkdir -p /var/solr/data
-	cp -r /usr/solr/marketplace-items /var/solr/data
+  mkdir -p /var/solr/data
+  cp -r /usr/solr/marketplace-items /var/solr/data
+else
+  cp -r /usr/solr/marketplace-items /var/solr/data
 fi
 
 if [ ! -d $SOLR_MARKETPLACE_CONCEPTS_DIR ]; then
-	mkdir -p /var/solr/data
-	cp -r /usr/solr/marketplace-concepts /var/solr/data
+  mkdir -p /var/solr/data
+  cp -r /usr/solr/marketplace-concepts /var/solr/data
+else
+  cp -r /usr/solr/marketplace-concepts /var/solr/data
 fi
 
 if [ ! -d $SOLR_MARKETPLACE_ACTORS_DIR ]; then
-	mkdir -p /var/solr/data
-	cp -r /usr/solr/marketplace-actors /var/solr/data
+  mkdir -p /var/solr/data
+  cp -r /usr/solr/marketplace-actors /var/solr/data
+else
+  cp -r /usr/solr/marketplace-actors /var/solr/data
 fi
 
 # Execute parent entrypoint script
