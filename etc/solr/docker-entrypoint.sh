1dd530fba36f3d13c4bbeb8000e9da86beb3da68
Tomasz Parkoła <605123+tparkola@users.noreply.github.com>
2023-09-26 10:36:44 +0200

    [SSHOC-395] Further upgrade of solr to version 9.3.0.

--- etc/solr/docker-entrypoint.sh
+++ etc/solr/docker-entrypoint.sh
@@ -4,17 +4,19 @@ SOLR_MARKETPLACE_ITEMS_DIR="/var/solr/data/marketplace-items"
 SOLR_MARKETPLACE_CONCEPTS_DIR="/var/solr/data/marketplace-concepts"
 SOLR_MARKETPLACE_ACTORS_DIR="/var/solr/data/marketplace-actors"
 
+/opt/solr/docker/scripts/init-var-solr
+
 if [ ! -d $SOLR_MARKETPLACE_ITEMS_DIR ]; then
-	cp -r /usr/solr/marketplace-items /var/solr/data
+	cp -R /usr/solr/marketplace-items /var/solr/data
 fi
 
 if [ ! -d $SOLR_MARKETPLACE_CONCEPTS_DIR ]; then
-	cp -r /usr/solr/marketplace-concepts /var/solr/data
+	cp -R /usr/solr/marketplace-concepts /var/solr/data
 fi
 
 if [ ! -d $SOLR_MARKETPLACE_ACTORS_DIR ]; then
-	cp -r /usr/solr/marketplace-actors /var/solr/data
+	cp -R /usr/solr/marketplace-actors /var/solr/data
 fi
 
 # Execute parent entrypoint script
-exec /opt/docker-solr/scripts/docker-entrypoint.sh "$@"
+exec /opt/solr/docker/scripts/docker-entrypoint.sh "$@"
