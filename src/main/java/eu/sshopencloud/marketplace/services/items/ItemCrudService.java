76ff13b3df2f5471a5a6604450c0e32c8fa394d5
Tomasz Parkoła <605123+tparkola@users.noreply.github.com>
2023-10-27 16:00:01 +0200

    [SSHOC-151] When contributor executes update discard approved flag/parameter in the context of not modified comparison.

--- src/main/java/eu/sshopencloud/marketplace/services/items/ItemCrudService.java
+++ src/main/java/eu/sshopencloud/marketplace/services/items/ItemCrudService.java
@@ -198,7 +198,10 @@ abstract class ItemCrudService<I extends Item, D extends ItemDto, P extends Pagi
             throws VersionNotChangedException {
         I currentItem = loadItemForCurrentUser(persistentId);
         ComparisonResult comparisonResult = ComparisonResult.UPDATED;
-        if (!draft && currentItem.getStatus() != ItemStatus.DRAFT && !(approved && currentItem.getStatus() != ItemStatus.APPROVED)) {
+        if (!draft && currentItem.getStatus() != ItemStatus.DRAFT &&
+                !(approved && LoggedInUserHolder.getLoggedInUser() != null &&
+                        !LoggedInUserHolder.getLoggedInUser().isContributor() &&
+                        currentItem.getStatus() != ItemStatus.APPROVED)) {
             comparisonResult = recognizePotentialChanges(currentItem, (ItemCore) itemCore);
             if (comparisonResult == ComparisonResult.UNMODIFIED) {
                 throw new VersionNotChangedException();
