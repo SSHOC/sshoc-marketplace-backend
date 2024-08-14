9860b2cb0c30b327a23163567cccafd876829ec8
Tomasz Parkoła <605123+tparkola@users.noreply.github.com>
2024-08-14 10:35:13 +0200

    [SSHOC-418] Upgrade to spring boot 2.7.18.

--- src/test/java/eu/sshopencloud/marketplace/controllers/actors/ActorControllerITCase.java
+++ src/test/java/eu/sshopencloud/marketplace/controllers/actors/ActorControllerITCase.java
@@ -19,7 +19,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
