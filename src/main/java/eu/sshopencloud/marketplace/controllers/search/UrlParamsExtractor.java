49c008e8beeaa5413621b472edf016b3e17329b5
woz <pwozniak@man.poznan.pl>
2026-04-13 15:23:56 +0200

    [SSHOC-486] Two new fq parameters added for contributor c.actor_id and c.actor_role

--- src/main/java/eu/sshopencloud/marketplace/controllers/search/UrlParamsExtractor.java
+++ src/main/java/eu/sshopencloud/marketplace/controllers/search/UrlParamsExtractor.java
@@ -10,6 +10,9 @@ import java.util.Map;
 @UtilityClass
 public class UrlParamsExtractor {
 
+    private static final String CONTRIBUTOR_RELATED_PARAM_PREFIX = "c.";
+    private static final String DYNAMIC_PROPERTY_RELATED_PARAM_PREFIX = "d.";
+
     public Map<String, List<String>> extractFilterParams(MultiValueMap<String, String> params) {
         Map<String, List<String>> filterParams = new HashMap<>();
         for (Map.Entry<String, List<String>> entry: params.entrySet()) {
@@ -22,15 +25,22 @@ public class UrlParamsExtractor {
     }
 
     public Map<String, String> extractExpressionParams(MultiValueMap<String, String> params) {
+        return extractExpressionParamsForPrefix(params,DYNAMIC_PROPERTY_RELATED_PARAM_PREFIX);
+    }
+
+    public Map<String, String> extractContributorRelatedSolrFilterQueryParams(MultiValueMap<String, String> params) {
+        return extractExpressionParamsForPrefix(params,CONTRIBUTOR_RELATED_PARAM_PREFIX);
+    }
+
+    public Map<String, String> extractExpressionParamsForPrefix(MultiValueMap<String, String> params, String prefix) {
         Map<String, String> expressionParams = new HashMap<>();
         for (Map.Entry<String, List<String>> entry: params.entrySet()) {
             String key = entry.getKey();
-            if (key.startsWith("d.")) {
+            if (key.startsWith(prefix)) {
                 if(!entry.getValue().get(0).isEmpty())
                     expressionParams.put(key.substring(2), entry.getValue().get(0));
             }
         }
         return expressionParams;
     }
-
 }
